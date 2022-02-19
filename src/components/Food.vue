643c77b6665c9dc4f54d0fefae3cfc44581e9186
Maria Eduarda dos Santos <mesps@cin.ufpe.br>
2022-02-19 14:08:11 -0300

    Generating random coordinates to food

---
+++ src/components/Food.vue
@@ -0,0 +1,41 @@
+<template>
+  <div v-show="xCoord && yCoord">
+    <q-icon name="las la-pizza-slice" color="orange-9" size="32px" :style="stylePosition"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Food',
+  props: ['topLimit', 'bottomLimit', 'rightLimit', 'leftLimit', 'width', 'height'],
+  data () {
+    return {
+      xCoord: 0,
+      yCoord: 0
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.generateCoordinate()
+    }, 200)
+  },
+  computed: {
+    stylePosition () {
+      return `left: ${this.xCoord}px; top: ${this.yCoord}px; z-index: 99`
+    }
+  },
+  methods: {
+    generateCoordinate () {
+      this.xCoord = this.randomIntFromInterval(0, this.width)
+      this.yCoord = this.randomIntFromInterval(0, this.height)
+    },
+    randomIntFromInterval(min, max) {
+      return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/Screen.vue
+++ src/pages/Screen.vue
@@ -1,21 +1,64 @@
 <template>
   <q-page class="flex flex-center">
-    <q-card flat bordered class="screen q-pa-md">
+    <q-card flat bordered class="screen q-pa-md" ref="screen">
       <span class="text-black text-bold text-h5" ref="foodCounter">
         Comida: {{ foodCounter }}
       </span>
+      <!-- {{ boundingClientRect }} -->
+      <food
+        :rightLimit="rightLimit"
+        :leftLimit="leftLimit"
+        :topLimit="topLimit"
+        :bottomLimit="bottomLimit"
+        :width="width"
+        :height="height"
+      />
     </q-card>
+    <q-resize-observer @resize="onResize" />
   </q-page>
 </template>
 
 <script>
+import food from 'components/Food'
 
 export default {
   name: 'PageIndex',
+  components: {
+    food
+  },
   data () {
     return {
-      foodCounter: 0
+      foodCounter: 0,
+      boundingClientRect: null
     }
+  },
+  computed: {
+    rightLimit () {
+      return (this.boundingClientRect && this.boundingClientRect.right) || 0
+    },
+    leftLimit () {
+      return (this.boundingClientRect && this.boundingClientRect.left) || 0
+    },
+    topLimit () {
+      return (this.boundingClientRect && this.boundingClientRect.top) || 0
+    },
+    bottomLimit () {
+      return (this.boundingClientRect && this.boundingClientRect.bottom) || 0
+    },
+    width () {
+      return (this.boundingClientRect && this.boundingClientRect.width) || 0
+    },
+    height () {
+      return (this.boundingClientRect && this.boundingClientRect.height) || 0
+    }
+  },
+  mounted () {
+    this.boundingClientRect = this.$refs.screen.$el.getBoundingClientRect()
+  },
+  methods: {
+    onResize (size) {
+      this.boundingClientRect = this.$refs.screen.$el.getBoundingClientRect()
+    },
   }
 }
 </script>
