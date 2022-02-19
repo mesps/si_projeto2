788916025520e547295bb3591c1d700a32d6a213
Maria Eduarda dos Santos <mesps@cin.ufpe.br>
2022-02-19 13:23:31 -0300

    Initial commit

---
+++ src/pages/Screen.vue
@@ -0,0 +1,30 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-card flat bordered class="screen q-pa-md">
+      <span class="text-black text-bold text-h5" ref="foodCounter">
+        Comida: {{ foodCounter }}
+      </span>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      foodCounter: 0
+    }
+  }
+}
+</script>
+
+<style lang="sass">
+ .screen
+  height: 400px
+  width: 600px
+  border: 2px solid #00023C
+  background-color: #DEDEDE
+
+</style>
